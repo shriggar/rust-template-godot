ad263ae527a01fd539e04220877822d6791aabfd
shriggar <shriggar@gmail.com>
2025-07-15 18:59:05 -0700

    vd8bf766t97yg

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,36 +1,37 @@
 use bevy::{prelude::*, state::app::StatesPlugin};
 use bevy_asset_loader::prelude::*;
-use gameplay::audio::GameAudio;
+// use gameplay::audio::GameAudio;
 use godot_bevy::prelude::{
-    godot_prelude::{gdextension, ExtensionLibrary},
-    GodotDefaultPlugins, *,
+    GodotDefaultPlugins,
+    godot_prelude::{ExtensionLibrary, gdextension},
+    *,
 };
-
+/*
 mod components;
 mod gameplay;
 mod level_manager;
 mod main_menu;
 mod scene_management;
-
+*/
 #[bevy_app]
 fn build_app(app: &mut App) {
     // This example uses most godot-bevy features
     app.add_plugins(GodotDefaultPlugins)
-        .add_plugins(StatesPlugin)
+     /*    .add_plugins(StatesPlugin)
         .init_state::<GameState>()
-        .add_loading_state(
+       .add_loading_state(
             LoadingState::new(GameState::Loading)
                 .continue_to_state(GameState::MainMenu)
                 .load_collection::<GameAudio>(),
-        )
+        )  
         .add_plugins((
             scene_management::SceneManagementPlugin,
             main_menu::MainMenuPlugin,
             level_manager::LevelManagerPlugin,
             gameplay::GameplayPlugin,
-        ));
+        )) */;
 }
-
+/*
 #[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
 enum GameState {
     #[default]
@@ -38,3 +39,4 @@ enum GameState {
     MainMenu,
     InGame,
 }
+*/
