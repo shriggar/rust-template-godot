11996102a0036f53db3b92b31bd26200df73c78a
shriggar <shriggar@gmail.com>
2025-08-25 20:52:26 -0700

    98hjreguthjyg35u986

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,44 +1,120 @@
-use bevy::{prelude::*, state::app::StatesPlugin};
-use bevy_asset_loader::prelude::*;
-// use gameplay::audio::GameAudio;
-use godot_bevy::prelude::{
-    GodotDefaultPlugins,
-    godot_prelude::{ExtensionLibrary, gdextension},
-    *,
+#![allow(unexpected_cfgs)] // silence potential `tracy_trace` feature config warning brought in by `bevy_app` macro
+use bevy::ecs::system::Query;
+use bevy::prelude::{
+    App, Commands, Component, Entity, IntoScheduleConfigs, Res, Time, Update, Without,
 };
+use bevy::transform::components::Transform;
+use godot::builtin::Vector2;
+use godot::classes::Sprite2D;
+use godot::global::godot_print;
+use godot_bevy::prelude::godot_prelude::ExtensionLibrary;
+use godot_bevy::prelude::godot_prelude::gdextension;
+use godot_bevy::prelude::{
+    GodotNodeHandle, GodotTransformSyncPlugin, Sprite2DMarker, bevy_app, main_thread_system,
+};
+use std::f32::consts::PI;
 
-mod components;
-mod gameplay;
-mod level_manager;
-mod main_menu;
-mod scene_management;
-
+// The build_app function runs at your game's startup.
+//
+// Entry point for the Godot-Bevy plugin. For more about the `#[bevy_app]` macro, see:
+// (https://docs.rs/godot-bevy-macros/0.6.1/godot_bevy_macros/attr.bevy_app.html)
+//
+// The #[bevy_app] macro is a wrapper around the Godot-Rust #[gdextension] macro:
+// (https://godot-rust.github.io/docs/gdext/master/godot/prelude/trait.ExtensionLibrary.html)
+//
+// Read more about the Bevy `App` parameter here:
+// (https://bevy.org/learn/quick-start/getting-started/apps/)
 #[bevy_app]
 fn build_app(app: &mut App) {
-    app.insert_resource(GodotTransformConfig::two_way())
-    
-    // This example uses most godot-bevy features
-    .add_plugins(GodotDefaultPlugins)
-     /*    .add_plugins(StatesPlugin)
-        .init_state::<GameState>()
-       .add_loading_state(
-            LoadingState::new(GameState::Loading)
-                .continue_to_state(GameState::MainMenu)
-                .load_collection::<GameAudio>(),
-        )  
-        .add_plugins((
-            scene_management::SceneManagementPlugin,
-            main_menu::MainMenuPlugin,
-            level_manager::LevelManagerPlugin,
-            gameplay::GameplayPlugin,
-        )) */;
+    // Print to the Godot console:
+    // (https://docs.rs/godot-core/0.3.1/godot_core/macro.godot_print.html)
+    godot_print!("Hello from Godot-Bevy!");
+
+    // Add the transform syncing plugin since we're using Transform components
+    app.add_plugins(GodotTransformSyncPlugin::default());
+
+    // A system is a normal Rust function.
+    //
+    // This line runs the `orbit_setup` and then the
+    // `orbit_system` functions every Godot render frame.
+    //
+    // Read more about Bevy's Entities, Components, and Systems here:
+    // (https://bevy.org/learn/quick-start/getting-started/ecs/).
+    //
+    // Godot-Bevy synchronizes the Bevy 'Update' schedule parameter with the
+    // Godot `_process` update cycle. There is also a `PhysicsUpdate` schedule
+    // parameter that is synchronized with the Godot `_physics_process` update cycle.
+    //
+    // Read more about other schedules provided by Godot-Bevy here:
+    // (https://bytemeadow.github.io/godot-bevy/scene-tree/timing.html).
+    app.add_systems(Update, (orbit_setup, orbit_system).chain());
 }
- 
-#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
-enum GameState {
-    #[default]
-    Loading,
-    MainMenu,
-    InGame,
+
+// Components are data that can be attached to entities.
+// This one will store the starting position of a Node2D.
+#[derive(Debug, Component)]
+struct InitialPosition {
+    pos: Vector2,
 }
- 
+
+// This component tracks the angle at which the Node2D is orbiting its starting position.
+#[derive(Debug, Component)]
+struct Orbiter {
+    angle: f32,
+}
+
+// This component is used as a marker to keep track of which nodes have been initialized.
+#[derive(Debug, Component)]
+struct NodeInitialized;
+
+// This system initializes Sprite2Ds with the required components to allow the orbit_system to manipulate them.
+#[main_thread_system]
+fn orbit_setup(
+    // Bevy Commands allow us to modify the state of the world, such as adding components to entities.
+    mut commands: Commands,
+
+    // Gather all Godot nodes without the `NodeInitialized` component.
+    // Also, include the Bevy entity identifier so we can add components to it.
+    mut uninitialized: Query<
+        (Entity, &mut GodotNodeHandle, &Sprite2DMarker),
+        Without<NodeInitialized>,
+    >,
+) {
+    for (entity, mut node_handle, _) in uninitialized.iter_mut() {
+        let sprite_node = node_handle.get::<Sprite2D>();
+        // The GodotNodeHandle allows us to call Godot methods such as `get_name()`.
+        godot_print!(
+            "Initializing node: {:?}",
+            sprite_node.get_name().to_string()
+        );
+        // Attach new components to the entity.
+        commands
+            .entity(entity)
+            .insert(InitialPosition {
+                pos: sprite_node.get_transform().origin,
+            })
+            .insert(Orbiter { angle: 0.0 })
+            .insert(NodeInitialized);
+    }
+}
+
+// This system orbits entities created above
+fn orbit_system(
+    // The `transform` parameter is a Bevy `Query` that matches all `Transform` components.
+    // `Transform` is a Godot-Bevy-provided component that matches all Node2Ds in the scene.
+    // (https://docs.rs/godot-bevy/latest/godot_bevy/plugins/core/transforms/struct.Transform.html)
+    mut transform: Query<(&mut Transform, &InitialPosition, &mut Orbiter)>,
+
+    // This is equivalent to Godot's `_process` `delta: float` parameter.
+    process_delta: Res<Time>,
+) {
+    // For single matches, you can use `single_mut()` instead:
+    // `if let Ok(mut transform) = transform.single_mut() {`
+    for (mut transform, initial_position, mut orbiter) in transform.iter_mut() {
+        let position2d = initial_position.pos + Vector2::from_angle(orbiter.angle) * 100.0;
+        transform.translation.x = position2d.x;
+        transform.translation.y = position2d.y;
+        orbiter.angle += process_delta.as_ref().delta_secs();
+        orbiter.angle %= 2.0 * PI;
+    }
+}
